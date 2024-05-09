f896369673eb7e8539d502564f0aa5e2ce90742f
camescasse <e.camescasse@gmail.com>
2024-05-09 19:11:30 -0400

    added create monsters tests

--- cypress/e2e/monsters.cy.ts
+++ cypress/e2e/monsters.cy.ts
@@ -31,5 +31,48 @@ describe('Monsters', () => {
         cy.get('.MuiAlert-message').should('contain.text', 'required');
       });
     });
+
+    it('should create a monster given all values except image', () => {
+      const noImageMonster: Monster = {
+        id: '',
+        name: 'monstrote',
+        attack: 10,
+        defense: 20,
+        hp: 30,
+        speed: 40,
+        imageUrl: '',
+      };
+
+      cy.get('[data-testid="monster-name"]').type(noImageMonster.name);
+      cy.get('[data-testid="hp-value"]').type(noImageMonster.hp.toString());
+      cy.get('[data-testid="attack-value"]').type(noImageMonster.attack.toString());
+      cy.get('[data-testid="defense-value"]').type(noImageMonster.defense.toString());
+      cy.get('[data-testid="speed-value"]').type(noImageMonster.speed.toString());
+      cy.get('[data-testid="btn-create-monster"]').click();
+
+      cy.get('[data-testid="card-monster-name"]').should('contain.text', noImageMonster.name);
+    });
+
+    it('should create a monster given all values and image', () => {
+      const noImageMonster: Monster = {
+        id: '',
+        name: 'monstrote',
+        attack: 10,
+        defense: 20,
+        hp: 30,
+        speed: 40,
+        imageUrl: '',
+      };
+
+      cy.get('[data-testid="monster-name"]').type(noImageMonster.name);
+      cy.get('[data-testid="hp-value"]').type(noImageMonster.hp.toString());
+      cy.get('[data-testid="attack-value"]').type(noImageMonster.attack.toString());
+      cy.get('[data-testid="defense-value"]').type(noImageMonster.defense.toString());
+      cy.get('[data-testid="speed-value"]').type(noImageMonster.speed.toString());
+      cy.get('[data-testid="monster-1"]').click();
+      cy.get('[data-testid="btn-create-monster"]').click();
+
+      cy.get('[data-testid="card-monster-name"]').should('contain.text', noImageMonster.name);
+    });
   });
 });
